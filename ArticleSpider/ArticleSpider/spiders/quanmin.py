0cac7c9cdb0c525bcf544607f5adb042e9b4898f
huangyisan <ahuangyisan@live.cn>
2018-07-17 12:43:57 +0800

    抓取所有页面，按人气排序

--- ArticleSpider/ArticleSpider/spiders/quanmin.py
+++ ArticleSpider/ArticleSpider/spiders/quanmin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.http import Request
+from urllib import parse
 
 
 
@@ -9,26 +10,22 @@ class QuanminSpider(scrapy.Spider):
     name = 'quanmin'
     allowed_domains = ['www.quanmin.tv']
 
-    start_urls = []
+    start_urls = ['https://www.quanmin.tv/game/all']
+
+
 
     def parse(self, response):
+        next_url = response.css(".list_w-paging_btn.list_w-paging_next::attr(href)").extract_first("")
+        if next_url:
+            self.parse_detail(response)
+            yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+
+    def parse_detail(self, response):
         # 单页
         anchor_list = response.css("span.common_w-card_host-name::text").extract()
         viewer_list = response.css("span.common_w-card_views-num::text").extract()
         anchor_viewer_dict = dict(zip(anchor_list, viewer_list))
-        pass
-
-    @classmethod
-    def get_page_size(cls, response):
-        last_page = response.css("div.list_w-videos_paging a::text").extract()[-2].strip()
-        return last_page
-
-    @classmethod
-    def append_start_urls(cls, page_size):
-        start_urls = []
-        for i in range(page_size):
-            start_urls.append('https://www.quanmin.tv/game/all?p={0}'.format(i))
-        return cls.start_urls
-
+        sorted(anchor_viewer_dict, key=lambda x:(anchor_viewer_dict.get(x)))
+        print(anchor_viewer_dict)
 
 
